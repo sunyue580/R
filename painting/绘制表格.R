9d0e7573e91b4d0b70ce676ed4489ea405288a1e
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2020-10-13 14:15:23 +0800

    Update 绘制表格.R

--- painting/绘制表格.R
+++ painting/绘制表格.R
@@ -1,1 +1,7 @@
-ggpubr::ggtexttable 
+1、ggpubr::ggtexttable   看帮助文档例子
+2、gt包
+3、kableExtra包：处理 HTML/LaTex 非常好。
+4、formattable：处理 HTML 自定义单元格填充非常好。
+5、DT包 或 reactable包：处理响应表（常用于 RMarkdown 和 Shiny）。
+6、flextable包：处理 Word 基于的表格。
+7、gtsummary包：有用的 gt 拓展包。
