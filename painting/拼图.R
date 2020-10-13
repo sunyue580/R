71fa360547974d9c849fd11af5df826debc4181e
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2020-10-13 20:15:19 +0800

    Update 拼图.R

--- painting/拼图.R
+++ painting/拼图.R
@@ -1,5 +1,7 @@
-##ggpubr——多个图公用同一个统一标度的legend
+1、ggpubr包——多个图共用同一个统一标度的legend
 library(ggpubr)
 ggarrange(p1, p2, p3,p4,ncol  = 2,nrow = 2,common.legend = TRUE,legend = "right")  
 ggsave("go_kegg_scatter.pdf",width = 12,height = 10)
 dev.off()
+
+2、customLayout包：绘图布局
