7bc8d2703a91bc2f73d91bc919831316cca7c6e6
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2020-10-24 22:30:45 +0800

    Create RStudio中文乱码解决.R

---
+++ RStudio中文乱码解决.R
@@ -0,0 +1,3 @@
+解决步骤：
+1、设置RStudio文本显示的默认编码：RStudio菜单栏的Tools -> Global Options -> code -> saving -> Default Text Encoding -> 选择UTF-8
+2、在打开文件的时候，选择File -> Reopen with encoding菜单，选择我们使用UTF-8编码打开文件，这样子就可以正确地显示中文了。
