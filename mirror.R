d85d85b55316021d08cf83433d2f79668a371856
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2021-01-28 15:53:11 +0800

    Update mirror.R

--- mirror.R
+++ mirror.R
@@ -1,5 +1,7 @@
 #设置CRAN镜像
 options(repos="https://mirrors.tongji.edu.cn/CRAN/")    
 #options(repos="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")  
+#options(repos="https://mirrors.ustc.edu.cn/CRAN/")
 #设置bioconductor镜像
 options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor/")
+#options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
