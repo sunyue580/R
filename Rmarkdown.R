6fa8eafcb2fc1017f269f7b19ac7be716eeaa705
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2021-03-04 19:33:13 +0800

    Update Rmarkdown.R

--- Rmarkdown.R
+++ Rmarkdown.R
@@ -1,2 +1,6 @@
-1、ctrl+shift+k：将rmd文件转换成其他格式（pdf/word/html）
-2、Ctrl+Alt+I：插入代码块
+1、Rmarkdown knit成pdf问题解决：
+(1) 下载包,并解压后放到存放包的路径——获取包路径.libPaths()
+(2) http://mirrors.ctan.org/macros/latex/contrib/fancyvrb.zip
+
+2、ctrl+shift+k：将rmd文件转换成其他格式（pdf/word/html）
+3、Ctrl+Alt+I：插入代码块
