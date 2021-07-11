8866a9dda980a0256a0faccbcceb53ac0b45885f
Sun Yue <55192298+sunyue580@users.noreply.github.com>
2021-07-11 23:09:20 +0800

    Update 探针注释.R

--- 芯片/探针注释.R
+++ 芯片/探针注释.R
@@ -1,3 +1,9 @@
 jmzeng1314/AnnoProbe
 jmzeng1314/idmap3
 hgu133a.db、hgu133plus2.db 
+
+#基因注释R包代替平台文件
+library(hgu133plus2.db)
+ls("package:hgu133plus2.db")
+ids=toTable(hgu133plus2SYMBOL)
+head(ids)
